576238cfd43877d9a4ddca200bfd4e04e36cbc73
k.sunaga-cq <k.sunaga@caraquri.com>
2019-02-12 16:12:09 +0900

    apiのエンドポイントを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :languages, only: %i(index new create post)
-  resources :products, only: %i(index new create post)
+  namespace :api do
+    namespace :v1 do
+      resources :languages, only: %i(index new create post)
+      resources :products, only: %i(index new create post)
+    end
+  end
 end
